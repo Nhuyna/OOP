8ed8482748a90792eb6b19fb900d4a84d9947a9f
ThLuyn <thuluyen0105@gmail.com>
2024-11-18 14:04:29 +0700

    L1

--- BookItem.java
+++ BookItem.java
@@ -1,21 +1,20 @@
 
-
-
-
 public class BookItem {
     protected String idBIteam;
     protected Books Book;
     protected boolean situation;
     protected Location location;
-   public BookItem(){
 
-   }
-   public BookItem(String idBItem,Books Book, boolean situation,Location location){
-     this.idBIteam=idBItem;
-     this.Book=Book;
-     this.situation=situation;
-     this.location=location;
-   }
+    public BookItem() {
+
+    }
+
+    public BookItem(String idBItem, Books Book, boolean situation, Location location) {
+        this.idBIteam = idBItem;
+        this.Book = Book;
+        this.situation = situation;
+        this.location = location;
+    }
 
     public void setIdBIteam(String idBIteam) {
         this.idBIteam = idBIteam;
@@ -28,15 +27,17 @@ public class BookItem {
     public Location getLocation() {
         return location;
     }
+
     public void setLocation(Location location) {
         this.location = location;
     }
+
     public void setLocation() {
-        if(situation==false){
-            this.location=null;
+        if (situation == false) {
+            this.location = null;
         }
     }
-    
+
     public Books getBook() {
         return Book;
     }
@@ -44,31 +45,69 @@ public class BookItem {
     public void setBook(Books Book) {
         this.Book = Book;
     }
+
     public void setSituation(boolean situation) {
         this.situation = situation;
     }
 
-     public boolean getSituation(){
+    public boolean getSituation() {
         return situation;
-     }    
-   
+    }
+
     @Override
-   public String toString(){
-   
-    return String.format("%5s%10s%10s%10s", getIdBIteam(),getBook().getName(),getSituation(),(getSituation())?location.toString():"null");
-   }
-   public static BookItem[] addElementBookItem(BookItem[] Sach, BookItem tmp) {
-    BookItem[] array = new BookItem[Sach.length+1];
-    System.arraycopy(Sach, 0, array, 0, Sach.length);
-    array[Sach.length] = tmp;
-    return array;}
-    public static BookItem SearchId(BookItem DS[],String a){
-        for(BookItem x : DS){
-             if(x.getIdBIteam().equals(a)){
-               
-              return x;
-             }
+    public String toString() {
+
+        return String.format("%5s%10s%10s%10s", getIdBIteam(), getBook().getName(), getSituation(),
+                (getSituation()) ? location.toString() : "null");
+    }
+
+    // Thêm
+    public static BookItem[] addElementBookItem(BookItem[] Sach, BookItem tmp) {
+        BookItem[] array = new BookItem[Sach.length + 1];
+        System.arraycopy(Sach, 0, array, 0, Sach.length);
+        array[Sach.length] = tmp;
+        return array;
+    }
+
+    // Sửa
+    public static BookItem updaBookItems(BookItem[] Sach, String idBIteam, Books Book, boolean situation, Location location) {
+        for(BookItem item : Sach) {
+            if (item.getIdBIteam().equals(idBIteam)) {
+                item.setBook(Book);
+                item.setSituation(situation);
+                item.setLocation(location);
+                return item;
+            }
         }
-          return null;
-      }
+        return null;
+    }
+
+    // Xóa
+    public static BookItem[] deleteBookItems(BookItem[] Sach, String id) {
+        int index = -1;
+        for (int i = 0; i < Sach.length; i++) {
+            if (Sach[i].getIdBIteam().equals(id)) {
+                index = i;
+                break;
+            }
+        }
+        if(index !=-1){
+            BookItem[] newArray = new BookItem[Sach.length -1 ];
+            System.arraycopy(Sach, 0, newArray, 0, index);
+            System.arraycopy(Sach, index + 1, newArray, index, Sach.length - index - 1);
+            return newArray;
+        }
+        return Sach;
+    }
+
+    // Tìm kiếm bằng ID
+    public static BookItem SearchId(BookItem DS[], String a) {
+        for (BookItem x : DS) {
+            if (x.getIdBIteam().equals(a)) {
+
+                return x;
+            }
+        }
+        return null;
+    }
 }
--- Books.java
+++ Books.java
@@ -4,16 +4,19 @@ public class Books {
     protected String name;
     protected Authors Author;
     protected Category category;
-    public Books (){
+
+    public Books() {
 
     }
-    public Books(String id,String name,Authors Author,Category category){
-        this.id=id;
-        this.name=name;
-        this.Author=Author;
-        this.category=category;
+
+    public Books(String id, String name, Authors Author, Category category) {
+        this.id = id;
+        this.name = name;
+        this.Author = Author;
+        this.category = category;
 
     }
+
     public void setId(String id) {
         this.id = id;
     }
@@ -33,32 +36,89 @@ public class Books {
     public Authors getAuthor() {
         return Author;
     }
-public void setAuthor(Authors Author) {
-    this.Author = Author;
-}
-   
+
+    public void setAuthor(Authors Author) {
+        this.Author = Author;
+    }
+
     public String getName() {
         return name;
     }
-   public void setName(String name) {
-       this.name = name;
-   } 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
-   public String toString(){
-    return String.format("%4s%45s%23s%23s",id,name,Author.name,category.NameC);
-   }
-   public static Books[] addElementBooks(Books[] Sach, Books tmp) {
-    Books[] array = new Books[Sach.length+1];
-    System.arraycopy(Sach, 0, array, 0, Sach.length);
-    array[Sach.length] = tmp;
-    return array;}
-    public static Books SearchId(Books DS[],String a){
-        for(Books x : DS){
-             if(x.getId().equals(a)){
-               
-              return x;
-             }
+    public String toString() {
+        return String.format("%4s%45s%23s%23s", id, name, Author.name, category.NameC);
+    }
+
+    // Thêm
+    public static Books[] addElementBooks(Books[] Sach, Books tmp) {
+        Books[] array = new Books[Sach.length + 1];
+        System.arraycopy(Sach, 0, array, 0, Sach.length);
+        array[Sach.length] = tmp;
+        return array;
+    }
+
+    // Sửa
+    public static void updateBook(Books[] Sach, Books id, Books newBooks) {
+        boolean found = false;
+        for (int i = 0; i < Sach.length; i++) {
+            if (Sach[i].getId().equals(id)) {
+                Sach[i] = newBooks;
+                found = true;
+                System.out.println("Da cap nhat thong tin sach cho ID: " + id);
+                break;
+            }
         }
-          return null;
-      }
+
+        if (!found) {
+            System.out.println("Khong tim thay sach voi ID: " + id);
+        }
+    }
+
+    // Xóa
+    public static Books[] removeBooks(Books[] Sach, String id) {
+        int index = -1;
+        for (int i = 0; i < Sach.length; i++) {
+            if (Sach[i].getId().equals(id)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index == -1) {
+            System.out.println("Khong tim thay sach voi ID: " + id);
+            return Sach;
+        }
+
+        Books[] newArray = new Books[Sach.length - 1];
+        System.arraycopy(Sach, 0, newArray, 0, index);
+        System.arraycopy(Sach, index + 1, newArray, index, Sach.length - index - 1);
+        System.out.println("Da xoa sach.\n");
+        return newArray;
+    }
+
+    // Tìm kiếm bằng id
+    public static Books SearchId(Books DS[], String a) {
+        for (Books x : DS) {
+            if (x.getId().equals(a)) {
+
+                return x;
+            }
+        }
+        return null;
+    }
+
+    // Tìm kiếm bằng tên
+    public static Books findByName(Books[] Sach, String bookName) {
+        for (Books x : Sach) {
+            if (x.getName().equalsIgnoreCase(bookName)) {
+                return x;
+            }
+        }
+        return null;
+    }
 }
